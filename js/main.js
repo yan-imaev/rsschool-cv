48e417cd4859cb88b8fdac073169988ba907f9df
Im Yan <yan.imaev@gmail.com>
2022-08-29 20:33:27 +0500

    feat: add some fix

--- js/main.js
+++ js/main.js
@@ -12,5 +12,17 @@ function toggleBurger() {
     mobileMenu.classList.toggle('toggle');
 }
 
-// Плавный переход по якорям в десктоп меню
+// Плавный переход по якорям
 
+let navItems = document.querySelectorAll('.main-page__header__nav ul li a'),
+    mobileNavItems = document.querySelectorAll('.mobile-nav li a');
+
+navItems.forEach((item) => href(item));
+mobileNavItems.forEach((item) => href(item));
+
+function href(item) {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(item.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+    })
+}
